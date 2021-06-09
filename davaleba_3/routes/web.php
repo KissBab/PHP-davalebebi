c6e83bd5c4108c7915b43bed13d5545a3748e203
KissBab <giorgi.babutsidze.1@btu.edu.ge>
2021-06-09 15:39:35 +0400

    edited

--- davaleba_3/routes/web.php
+++ davaleba_3/routes/web.php
@@ -12,6 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
+Route::get('/posts/create', [\App\Http\Controllers\PostController::class, 'create']);
+Route::post('/posts/store', [\App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{id}/update', [\App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{id}/delete', [\App\Http\Controllers\PostController::class, 'delete'])->name('posts.delete');
+Route::get('/posts/{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
 
 Route::get('/', function () {
     return view('welcome');
