5b57d1d7c38ebf228c24c673f7195455aa4880f0
KissBab <giorgi.babutsidze.1@btu.edu.ge>
2021-06-09 19:28:56 +0400

    tag

--- davaleba_4/routes/web.php
+++ davaleba_4/routes/web.php
@@ -12,16 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/list', [\App\Http\Controllers\PostController::class, 'list'])->name('posts.list');
-Route::get('/posts/create', [\App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
-Route::post('/posts/store', [\App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}/update', [\App\Http\Controllers\PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{id}/delete', [\App\Http\Controllers\PostController::class, 'delete'])->name('posts.delete');
-Route::get('/posts/{id}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
 
 Route::get('/', function () {
-    $posts =\App\Models\Post::paginate(10);
-    return view('test.test', compact('posts'));
+    return view('welcome');
 });
